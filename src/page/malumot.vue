cccb0c8d244672bb306a586912d1a7b9ab41a7ce
Firdavs <124765174+Firdavsxonshezodov@users.noreply.github.com>
2023-07-14 19:18:03 +0300

    bostir

--- src/page/malumot.vue
+++ src/page/malumot.vue
@@ -7,32 +7,40 @@
 
             <img :src="srt" alt="" class="">
 
-            
+
 
 
         </div>
         <div class="ikki shadow-5">
-            <p class="fa-heart1"> <i class="fas fa-heart fs-5" :class="{ active: isActive }" @click="isActive = true"></i> В желания</p>
-        <p class="d-flex" >   <i class="fas fa-star text-warning" style="margin-left:15px;"></i>  0.0 Оценок пока нет<p class=" text-Secondary "  style="  padding-left:15px; font-size: 15px;"> Нет заказов</p> </p>
-    
+            <p class="fa-heart1"> <i class="fas fa-heart fs-5" :class="{ active: isActive }" @click="isActive = true"></i> В
+                желания</p>
+            <p class="dft d-flex fs-6" > <i class="fas fa-star text-warning fs-6"></i>  1.0 ( Оценок пока нет )
+            <p class=" text-Secondary " style="padding-left:15px; font-size: 15px;"> Нет заказов</p>
+            </p>
 
-<p class="pp1 fs-4 text-dark p-3"> {{ title }}</p>
 
- 
+            <p class="pp1 fs-4 text-dark p-3"> {{ title }}</p>
 
-<hr>
-<h5 class="hh5">Количество:</h5>
-<p class="p2"> В наличии 42</p>
-             <div class="d1 shadow-3">
+<div class="ddd3">
+    <span class="s1">Доставка:</span>
+    <span class="s1">Доставка:1</span>
+    <span>Продавец:</span>
+    <span>1 день, бесплатно</span>
+</div>
+
+            <hr>
+            <h5 class="hh5">Количество:</h5>
+            <p class="p2"> В наличии 42</p>
+            <div class="d1 shadow-3">
                 <span @click="srt1" ref="nozim" class="cursor">-</span>
                 <h2>{{ count }} </h2>
                 <span type="button" @click="count++">+</span>
             </div>
             <p class="p1">Цена:</p>
-            <h5 class="h5">{{ price*count }} $
+            <h5 class="h5">{{ price * count }} $
                 <h5 class="p3">3 472 000 сум</h5>
                 <h5 class="hhh5">Чилля Базар</h5>
-</h5>
+            </h5>
         </div>
 
 
@@ -45,11 +53,11 @@
 </template>
 
 <script setup>
- 
+
 import header1 from '../components/Header.vue'
 import { RouterLink } from 'vue-router';
 import axios from 'axios'
-import { ref,onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 let router = useRoute()
 let parmId = router.params.id
@@ -62,23 +70,23 @@ axios.get(`https://fakestoreapi.com/products/${parmId}`)
         srt.value = resp1.data.image
         price.value = resp1.data.price
         title.value = resp1.data.title
- 
+
     })
 
 
 const nozim = ref('')
-        let span = document.querySelector('.cursor')
+let span = document.querySelector('.cursor')
 
-let srt1 = ()=>{
+let srt1 = () => {
     console.log(nozim._rawValue);
     count.value--
-   if (count.value === 0) {
-    // span.classList.add('nimadir')
-    nozim._rawValue.style.visibility = 'collapse';
-   }
-    if(count.value > 1){
-    nozim._rawValue.style.visibility = 'visible';
-   }
+    if (count.value === 0) {
+        // span.classList.add('nimadir')
+        nozim._rawValue.style.visibility = 'collapse';
+    }
+    if (count.value > 1) {
+        nozim._rawValue.style.visibility = 'visible';
+    }
 }
 
 
@@ -93,16 +101,20 @@ const count = ref(1)
 <style scoped>
 .active {
     color: red;
+    cursor: pointer;
 }
+
 .nimadir {
     pointer-events: none;
 }
+
 .nimadir1 {
-   pointer-events: all;
+    pointer-events: all;
 }
- .cursor{
+
+.cursor {
     cursor: pointer;
- }
+}
 
 .ikki {
     width: 69%;
@@ -125,7 +137,7 @@ img {
     height: 80vh;
     text-align: center;
     margin: 5vh;
-    border-radius: 15px;    
+    border-radius: 15px;
 }
 
 .d1 {
@@ -137,13 +149,13 @@ img {
     border-radius: 10px;
     border: solid gray 1px transparent;
     position: absolute;
-    top: 55vh;
-    left: 20vh;
+    top: 43vh;
+    left: 19.5vh;
     border: solid grey 1.5px;
-    
-     
+
+
 }
- 
+
 
 .i {
     margin-top: 25px;
@@ -165,60 +177,120 @@ h6 {
 span {
     font-size: 30px !important;
 }
-.h5{
+
+.h5 {
     position: absolute;
-    top: 70vh;
-    left: 20vh;  
+    top: 56vh;
+    left: 20vh;
+    color: black;
+    font-size: 22px;
 }
-.hh5{
+
+.hh5 {
     position: absolute;
-    top: 50vh;
-    left: 20vh;  
+    top: 37vh;
+    left: 20vh;
 }
-.p1{
+
+.p1 {
     position: absolute;
-    top:66vh;
-    left:20vh;
+    top: 51.5vh;
+    left: 20vh;
     color: black;
 }
-.p2{
+
+.p2 {
     position: absolute;
-    top:57vh;
-    left:50vh;
-    color: rgb(139, 232, 0);
+    top: 45vh;
+    left: 50vh;
+    color: #00c853;
+
 }
-.p3{
+
+.p3 {
     position: absolute;
     font-size: 15px;
     top: -1px;
-    left:20vh;
+    left: 20vh;
     width: 100px;
     text-decoration: line-through;
     color: grey;
-   
+
 }
-.hhh5{
+
+.hhh5 {
     position: absolute;
     font-size: 15px;
     top: -1px;
-    left:35vh;
+    left: 35vh;
     width: 110px;
     height: 25px;
     background-color: blueviolet;
     color: #fff;
-    padding: 3px;
+    padding: 3.5px;
+    padding-left: 7.5px;
+    border-radius: 5px;
+    
 }
-.pp1{
-    padding-left:20vh !important;
+
+.pp1 {
+    padding-left: 20vh !important;
 }
- .fa-star{
-    padding-left:18vh !important;
- }
- .fa-heart1{
+
+.fa-star {
+    padding-left: 18vh !important;
+  
+}
+
+.fa-heart1 {
     padding-left: 120vh;
-   position: absolute;
-   top: 2vh;
- }
+    position: absolute;
+    top: 4vh;
+}
+
+.dft {
+padding-left: 2vh;
+padding-top: 4vh;
+}
+.ddd3 span{
+    font-size: 14px !important;
+    padding-left: 10vh;
+
+}
+.ddd3 span:hover{
+    color: black;
+    cursor: pointer;
+}
+.ddd3{
+    width: 50vh;
+    height: 10vh; 
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap-reverse;
+  align-items: center;
+  margin-left: 8vh;
+}
+.s1{
+    padding-right:3vh !important;
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </style>
 
  
